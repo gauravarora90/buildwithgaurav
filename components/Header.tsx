6164ed8437d5fa6f166a1335a5d9b0a0c1826333
Gaurav Arora <gaurav.arora90@gmail.com>
2026-05-06 11:55:29 +0530

    Show header title on mobile

--- components/Header.tsx
+++ components/Header.tsx
@@ -20,9 +20,7 @@ const Header = () => {
             <Logo />
           </div>
           {typeof siteMetadata.headerTitle === 'string' ? (
-            <div className="hidden h-6 text-2xl font-semibold sm:block">
-              {siteMetadata.headerTitle}
-            </div>
+            <div className="h-6 text-lg font-semibold sm:text-2xl">{siteMetadata.headerTitle}</div>
           ) : (
             siteMetadata.headerTitle
           )}
